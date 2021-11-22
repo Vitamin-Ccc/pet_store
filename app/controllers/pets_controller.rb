ae5534ea96386dda593d32a97910d1fbf4a7a298
Sky Lin <linsky519@gmail.com>
2021-11-22 00:21:50 -0700

    controller and seeds completed

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,22 +1,61 @@
 class PetsController < ApplicationController
+  before_action :set_store, only: [:show, :edit, :update, :destroy]
+
+  # Read
   def index
+    @pets = Pet.all
+    render component: "pets", props: { pets:@pets }
   end
 
   def show
+    @pet = Pet.find(params[:id])
+    render component: "pet", props: { pet: @pet }
   end
 
+  # Create
   def new
+    render component: "petNew"
   end
 
   def create
+    @pet = Pet.new(pets_params)
+    if @pet.save
+      redirect_to pets_path
+    else
+      render component: "petNew", props: { pet: @pet }
+    end
   end
 
+  # Update
   def edit
+    @pet = Pet.find(params[:id])
+    render component: "petEdit", props: { pet: @pet }
   end
 
   def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pets_path
+    else
+      render component: "petEdit", props: { pet: @pet }
+    end
   end
 
+  # Delete/Destroy
   def destroy
+    pet = Pet.find(params[:id])
+    pet.destroy
+    redirect_to pets_path
   end
+
+  private
+
+  def pets_params
+    params.require(:pet).require(:name)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.destroy_all
+
+dog = Pet.create(name: "Dogs")
+cat = Pet.create(name: "Cats")
+
+dog.breeds.create(name: "Australian Shepherd", description: "The Australian Shepherd is a breed of herding dog from the United States. Originally used solely as a herding dog, the Australian Shepherd has become one of the most popular companion dog breeds in North America.")
+dog.breeds.create(name: "Border Collie", description: "The Border Collie is a working and herding dog breed. They come from the Anglo-Scottish border region and are used to herd livestock, specifically sheep.")
+dog.breeds.create(name: "Siberian Husky", description: "The Siberian Husky is a medium-sized working sled dog breed. The breed belongs to the Spitz genetic family.")
+
+
+cat.breeds.create(name: "Ragdoll", description: "The Ragdoll is a cat breed with a color point coat and striking blue eyes. Their form is large and muscular and their coat is silky soft and semi-longhair.")
+cat.breeds.create(name: "Siamese", description: "The Siamese cat is one of the first distinctly recognized breeds of Asian cat. Derived from the Wichianmat landrace, one of several varieties of cat native to Thailand, the original Siamese became one of the most popular breeds in Europe and North America in the 19th century.")
+cat.breeds.create(name: "American Shorthair", description: "The American Shorthair is a breed of domestic cat believed to be descended from European cats brought to North America by early settlers to protect valuable cargo from mice and rats.")
+
